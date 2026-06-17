99e7bce337eb8cbc3e3eb589be835d0e71b31320
NgoHuy05 <23020610@vnu.edu.vn>
2026-06-17 17:29:59 +0700

    feat: logic tranfer monet, watch history

---
+++ backend/api/controllers/TransactionController.js
@@ -0,0 +1,89 @@
+module.exports = {
+  transferMoney: async (req, res) => {
+    try {
+      const { phone, amount } = req.body;
+
+      const senderPocket = await Pocket.findOne({ owner: req.user.id });
+      const receiverCustomer = await Customer.findOne({ phone });
+
+      if (!phone || !amount || amount <= 0) {
+        return res.error(4000); //   4000: 'missing required fields',
+      }
+
+      if (!receiverCustomer) {
+        return res.error(2004); //  2004: 'receiver not found',
+      }
+
+      if (req.user.id === receiverCustomer.id) {
+        return res.error(2003); // 2003: 'cannot transfer to yourself',
+      }
+
+      const receiverPocket = await Pocket.findOne({ owner: receiverCustomer.id });
+
+      if (!receiverPocket) {
+        return res.error(2004); // 2004 receiver pocket not found
+      }
+
+      if (senderPocket.balance < amount) {
+        return res.error(2001); // 2001: 'insufficient balance',
+      }
+
+      await Pocket.updateOne({ id: senderPocket.id })
+        .set({ balance: senderPocket.balance - amount });
+
+      await Pocket.updateOne({ id: receiverPocket.id })
+        .set({ balance: receiverPocket.balance + amount });
+
+      const transaction = await Transaction.create({
+        sender: req.user.id,
+        receiver: receiverCustomer.id,
+        amount,
+        status: 'success'
+      }).fetch();
+
+      return res.ok(transaction);
+
+    } catch (error) {
+      return res.error(500);
+    }
+  },
+
+  getAllHistoryTransaction: async (req, res) => {
+    try {
+      const history = await Transaction.find({
+        or: [
+          { sender: req.user.id },
+          { receiver: req.user.id }
+        ]
+      });
+
+      return res.ok(history);
+    } catch (error) {
+      return res.error(500);
+    }
+  },
+
+  getReceivedHistory: async (req, res) => {
+    try {
+      const history = await Transaction.find({
+        receiver: req.user.id
+      });
+
+      return res.ok(history);
+    } catch (error) {
+      return res.error(500);
+    }
+  },
+
+  getSentHistory: async (req, res) => {
+    try {
+      const history = await Transaction.find({
+        sender: req.user.id
+      });
+
+      return res.ok(history);
+    } catch (error) {
+      return res.error(500);
+    }
+  },
+};
