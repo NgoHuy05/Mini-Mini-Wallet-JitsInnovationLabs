b0e936165317a045046e5f6387003bf2d0e282b6
NgoHuy05 <23020610@vnu.edu.vn>
2026-06-17 16:39:39 +0700

    config: init routes pocket

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -2,4 +2,6 @@ module.exports.routes = {
   'POST /auth/login': 'AuthController.login',
   'POST /auth/register': 'AuthController.register',
 
+  'POST /pocket/me': 'PocketController.getMyPocket',
+  'POST /pocket/balance': 'PocketController.getBalance',
 };
